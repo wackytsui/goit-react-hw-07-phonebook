5cd40ce84d9cc10d70fe16242a1f2877f6a33cd4
Mark Allan Altavano <143721243+wackytsui@users.noreply.github.com>
2024-09-18 11:40:03 +0800

    import

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,10 +1,10 @@
+import { createAction } from '@reduxjs/toolkit';
 
 
 export const ADD_CONTACT = 'contacts/addContact';
 export const DELETE_CONTACT = 'contacts/deleteContact';
 export const SET_FILTER = 'filter/setFilter';
 
-import { createAction } from '@reduxjs/toolkit';
 
 export const addContact = createAction('contacts/addContact');
 export const deleteContact = createAction('contacts/deleteContact');
