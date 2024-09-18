3df59d9adfbcbaa284230939c3c7e61f586d57ca
Mark Allan Altavano <143721243+wackytsui@users.noreply.github.com>
2024-09-18 11:36:09 +0800

    Update reducers.js

--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -1,4 +1,5 @@
-
+import { createReducer } from '@reduxjs/toolkit';
+import { addContact, deleteContact, setFilter } from './actions';
 
 const initialContactsState = [
   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
@@ -6,11 +7,6 @@ const initialContactsState = [
   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
-
-const initialFilterState = '';
-
-import { createReducer } from '@reduxjs/toolkit';
-import { addContact, deleteContact, setFilter } from './actions';
 
 export const contactsReducer = createReducer(initialContactsState, {
   // this syntax refers to dynamic object keys and not arrays
